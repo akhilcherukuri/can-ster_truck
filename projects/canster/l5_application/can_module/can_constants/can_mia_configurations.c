802ee9a02a20da266a7810fa42d04f5b982b3002
ganeshram2493 <ganeshram2493@gmail.com>
2020-04-24 00:35:46 -0700

    Updated dbc_mia_replacement_SENSOR_SONARS to {{0}, 0, 0, 0}

--- projects/canster/l5_application/can_module/can_constants/can_mia_configurations.c
+++ projects/canster/l5_application/can_module/can_constants/can_mia_configurations.c
@@ -5,13 +5,15 @@
 // Sensor
 const uint32_t dbc_mia_threshold_SENSOR_HEARTBEAT = 3000;
 const uint32_t dbc_mia_threshold_SENSOR_SONARS = 3000;
+
 const dbc_SENSOR_HEARTBEAT_s dbc_mia_replacement_SENSOR_HEARTBEAT = {{0}, SENSOR_HEARTBEAT_cmd_REBOOT};
-const dbc_SENSOR_SONARS_s dbc_mia_replacement_SENSOR_SONARS = {{0}, 10, 10, 10};
+const dbc_SENSOR_SONARS_s dbc_mia_replacement_SENSOR_SONARS = {{0}, 0, 0, 0};
 
 // Driver
 const uint32_t dbc_mia_threshold_DRIVER_HEARTBEAT = 3000;
 const uint32_t dbc_mia_threshold_MOTOR_SPEED = 3000;
 const uint32_t dbc_mia_threshold_MOTOR_STEERING = 3000;
+
 const dbc_DRIVER_HEARTBEAT_s dbc_mia_replacement_DRIVER_HEARTBEAT = {{0}, DRIVER_HEARTBEAT_cmd_REBOOT};
 const dbc_MOTOR_SPEED_s dbc_mia_replacement_MOTOR_SPEED = {{0}, 3};
 const dbc_MOTOR_STEERING_s dbc_mia_replacement_MOTOR_STEERING = {{0}, 0};
